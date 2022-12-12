7b0178ef1189235adb4e540338e440cd60fb70fb
unknown <jeyhunny@code.edu.az>
2022-12-13 01:29:23 +0400

    first task

---
+++ Heap_Stack/Program.cs
@@ -0,0 +1,131 @@
+﻿//int a = 5;
+//int b = a;
+//b = 10;
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+
+
+//int[] arr = { 1, 2, 3 };
+//int[] arr2 = arr;
+
+//arr2[0] = 100;
+//Console.WriteLine(arr[0]);
+//Console.WriteLine(arr2[0]);
+
+
+//int n = 5;
+
+//Test(n);
+//Console.WriteLine(n);
+
+//static void Test(int n)
+//{
+//    n = 100;
+//    Console.WriteLine(n);
+//}
+
+//Test2(arr);
+//Console.WriteLine(arr[0]);
+// static void Test2(int[] arr)
+//{
+//    arr[0] = 200;
+//    Console.WriteLine(arr[0]);
+
+//}
+
+//int n = 5;
+
+//Test(ref n);
+//Console.WriteLine(n);
+
+//static void Test(ref int n)
+//{
+//    n = 100;
+//    Console.WriteLine(n);
+//}
+
+//int n;
+//Test(out n);
+//Console.WriteLine(n);
+
+//static void Test(out int n)
+//{
+//    n = 100;
+//    Console.WriteLine(n);
+//}
+
+//string name = "Jeyhun";
+//string name2 = name;
+//name2 = "Kenan";
+//Console.WriteLine(name);
+//Console.WriteLine(name2);
+
+
+//string address = "Buzovna";
+
+//int result = address.Length;
+//Console.WriteLine(result);
+
+//C/onsole.WriteLine(address.StartsWith("t"));/
+
+//Console.WriteLine(address.EndsWith("a"));
+
+//if (address.EndsWith("a"))
+//{
+//    Console.WriteLine("Yes");
+//}
+//else
+//{
+//    Console.WriteLine("No");
+//}
+
+//if (address.Contains("i"))
+//{
+//    Console.WriteLine("Yes");
+//}
+//else
+//{
+//    Console.WriteLine("No");
+//}
+
+//var res = address.Split(",");
+//foreach (var item in res)
+//{
+//    Console.WriteLine(item);
+//}
+
+//Console.WriteLine(address.ToLower());
+
+////address = "Jeyhun";
+////Console.WriteLine(address.ToUpper());
+
+//string address = "roya";
+//var letter = address[0].ToString().ToUpper();
+//var arr = address.ToCharArray();
+//arr[0] = char.Parse(letter);
+//string result = "";
+//foreach (var item in arr)
+//{
+//    result += item;
+
+//Console.WriteLine(result);
+//}
+
+//string name = "Jeyhun";
+////Console.WriteLine(name.ToUpper());
+
+////Console.WriteLine(name.Substring(1));
+
+////Console.WriteLine(name.Trim());
+//if (!string.IsNullOrEmpty(name))
+//{
+//    Console.WriteLine("Yes");
+//}
+//else
+//{
+//    Console.WriteLine("No");
+
+
+/////Task Homework
+//string name = "Jeyhun";
+//Console.WriteLine(name.Replace ("Jeyhun","Nuhyej"));
